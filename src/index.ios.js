f7a225c291d9dc3420def6ce3c66763d80e251a2
Noitidart Saab <noitidart@Noitidarts-Mac-mini.local>
2018-05-22 01:07:48 -0700

    add ios support, and changed entry to src dir so it works cross platform

--- src/index.ios.js
+++ src/index.ios.js
@@ -2,38 +2,34 @@
 
 import { ActionSheetIOS } from 'react-native'
 
-type PopupMenuItem = {
-    id?: any,
-    label?: string
-}
+type PopupMenuItem = { id?: any, label?: string }
 type OnPopupMenuItemSelect = (selectedPopupMenuItem: PopupMenuItem) => void
-type PopupMenuOptions = {
+type PopupMenuOptions = {|
+    cancelLabel?: string, // defaults to "Cancel"
+    onCancel?: () => void
+|}
 
-}
+async function showPopupMenu(items: PopupMenuItem[], onSelect: OnPopupMenuItemSelect, ignoreThisIsAnchorOnAndroid, { cancelLabel='Cancel', onCancel }: PopupMenuOptions={}): Promise<void> {
+    // const actionTitles = this.props.hiddenButtons.map(btn => btn.props.title);
+    // actionTitles.push(this.props.cancelButtonLabel);
 
-function showPopupMenu(onSelect, options, items) {
-    alert('NOT YET IMPLEMENTED');
+    const actionSheetItems = items.map(item => item.label);
+    actionSheetItems.unshift(cancelLabel);
 
-    // // const actionTitles = this.props.hiddenButtons.map(btn => btn.props.title);
-    // // actionTitles.push(this.props.cancelButtonLabel);
+    const selectedIndex: number = await new Promise(resolve =>
+        ActionSheetIOS.showActionSheetWithOptions({
+            options: actionSheetItems,
+            cancelButtonIndex: 0
+        }, resolve)
+    );
 
-    // const cancelButtonIndex = items.length - 1;
-    // ActionSheetIOS.showActionSheetWithOptions(
-    //     {
-    //         options: items.map(item => item.label),
-    //         cancelButtonIndex
-    //         // TODO: respect noCancel
-    //         // TODO: what hapens on dismiss? does it fire callback with cancelButtonindex?
-    //     },
-    //     function(index: number) {
-    //         if (index === cancelButtonIndex) {
-    //             if (onCancel) onCancel();
-    //             // handleHiddenItemPress('itemSelected', index);
-    //         } else {
-    //             onSelect(items[index]);
-    //         }
-    //     }
-    // )
+    console.log('selectedIndex:', selectedIndex);
+
+    if (selectedIndex === 0) {
+        if (onCancel) onCancel();
+    } else {
+        onSelect(items[selectedIndex - 1]);
+    }
 }
 
 export type { PopupMenuItem, OnPopupMenuItemSelect, PopupMenuOptions }
